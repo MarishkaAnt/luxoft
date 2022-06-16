fb3530a4e73a2a946ae6015e2e9f0e9f62d5190e
Mariia Antonova <mariia_antonova@epam.com>
2022-06-16 15:37:23 +0300

    other implementations of Set added

--- src/main/java/com/example/SetOrderTesting.java
+++ src/main/java/com/example/SetOrderTesting.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class SetOrderTesting {
     public static void main(String[] args) {
-        Set<Integer> ints = new TreeSet<>();
+        Set<Integer> ints = new ConcurrentSkipListSet<>();
         ints.add(5);
         ints.add(60);
         ints.add(48);
@@ -15,6 +18,7 @@ public class SetOrderTesting {
         ints.add(0);
         System.out.println(ints);
         // HashSet = [48, 0, 1, 5, 22, 60, 30]
-        // TreeSet = [0, 1, 5, 22, 30, 48, 60]
+        // TreeSet, ConcurrentSkipListSet = [0, 1, 5, 22, 30, 48, 60]
+        // LinkedHashSet, CopyOnWriteArraySet = [5, 60, 48, 30, 22, 1, 0]
     }
 }
